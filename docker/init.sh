d51c7df5701fa314a3c799848c5515860ed02f20
Your Git's Name <Your Email>
2018-11-08 07:56:38 +0700

    Setup docker

---
+++ docker/init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DOCUMENT_ROOT="/var/www/html/laravel-init-docker"
+
+echo $DOCUMENT_ROOT
+
+# Change Mode for LMS_ADMIN
+chmod -R 777 "$DOCUMENT_ROOT/storage/logs/"
+chmod -R 777 "$DOCUMENT_ROOT/storage/framework/"
+chmod -R 777 "$DOCUMENT_ROOT/bootstrap"
+
+/my_init
